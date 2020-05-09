07e138f46b09e1a702b9df8f1e701db20a38defa
bounty-pay-code <65063615+bounty-pay-code@users.noreply.github.com>
2020-05-09 08:18:59 +0100

    Create logger.php

---
+++ logger.php
@@ -0,0 +1,13 @@
+<?php
+
+$data = array(
+  'IP'        =>  $_SERVER["REMOTE_ADDR"],
+  'URI'       =>  $_SERVER["REQUEST_URI"],
+  'METHOD'    =>  $_SERVER["REQUEST_METHOD"],
+  'PARAMS'    =>  array(
+      'GET'   =>  $_GET,
+      'POST'  =>  $_POST
+  )
+);
+
+file_put_contents('bp_web_trace.log', date("U").':'.base64_encode(json_encode($data))."\n",FILE_APPEND   );
